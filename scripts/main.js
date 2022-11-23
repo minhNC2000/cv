82ad0f935ca013b8a037bbd92c0cbf4a3befd398
minhnc2000 <mrminhpro1104@gmail.com>
2022-11-24 00:50:49 +0700

    v1.2.2

--- scripts/main.js
+++ scripts/main.js
@@ -22,3 +22,21 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+//------------------ Go to top
+let mybutton = document.getElementById("gototop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
