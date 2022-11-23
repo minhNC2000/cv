464edc2c086b7419626ae0fc41f172cb167195bb
minhnc2000 <mrminhpro1104@gmail.com>
2022-11-24 00:19:39 +0700

    v1.2

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,24 @@
 // Animations
 AOS.init({
-  anchorPlacement: 'top-left',
-  duration: 1000
+  anchorPlacement: "top-left",
+  duration: 1000,
 });
 
 // Add your javascript here
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.querySelector("header");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    navbar.style.backgroundColor = "#ffffff";
+    navbar.style.zIndex = 9999;
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
