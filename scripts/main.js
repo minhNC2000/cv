eee9e85f662e23588bb2034c7bc684ba0103de83
minhnc2000 <mrminhpro1104@gmail.com>
2022-11-24 22:16:18 +0700

    v1.0

--- scripts/main.js
+++ scripts/main.js
@@ -11,32 +11,13 @@ window.onscroll = function () {
 };
 
 var navbar = document.querySelector("header");
+console.log(navbar);
 var sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
-    navbar.style.backgroundColor = "#ffffff";
-    navbar.style.zIndex = 9999;
   } else {
     navbar.classList.remove("sticky");
   }
-}
-
-//------------------ Go to top
-let mybutton = document.getElementById("gototop");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
 }
